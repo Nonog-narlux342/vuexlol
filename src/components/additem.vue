78355fee154110692f1cdfe50742be4240631b9d
Nonog-narlux342 <68525348+Nonog-narlux342@users.noreply.github.com>
2020-10-09 21:42:40 +0700

    init

---
+++ src/components/additem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field v-model="Name" label="Name" required></v-text-field>
+      <v-text-field
+        v-model="Review"
+        style="1"
+        label="Text Review"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="Point"
+        type="number"
+        style="1"
+        label="Point"
+        required
+      ></v-text-field>
+      <v-checkbox
+        v-model="status"
+        value="Too Good"
+        label="Too Good"
+        required
+      ></v-checkbox>
+      <v-checkbox
+        v-model="status"
+        value="Too Bad"
+        label="Too Bad"
+        required
+      ></v-checkbox>
+      <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Validate
+      </v-btn>
+      <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    Name: "",
+    Review: "",
+    status: "",
+    Point: "",
+    select: null,
+    checkbox: false,
+  }),
+  methods: {
+    validate() {
+      if (this.Name) {
+        let payload = {
+          Review: this.Review,
+          Point: this.Point,
+          status: this.status,
+          Name: this.Name
+        };
+        this.$store.dispatch("setItem", payload);
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    page() {
+      this.$store.state.page = 2;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/datatable.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <v-row no-gutters>
+      <v-col md="2"><v-card class="pa-2 info white--text" outlined> id</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2 info white--text" outlined> Name</v-card></v-col>
+      <v-col md="4"><v-card class="pa-2 info white--text" outlined> Text Review</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2 info white--text" outlined> Point</v-card></v-col>
+      <v-col md="2"> <v-card class="pa-2 info white--text" outlined> status</v-card></v-col>
+    </v-row>
+    <v-row v-for="(items, index) in $store.getters.items" :key="index"  no-gutters>
+      <v-col md="2"><v-card class="pa-2" outlined> {{ index + 1 }}</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2" outlined> {{ items.Name }}</v-card></v-col>
+      <v-col md="4"><v-card class="pa-2" outlined> {{ items.Review }}</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2" outlined> {{ items.Point }}</v-card></v-col>
+      <v-col md="2"><v-card class="pa-2" outlined> {{ items.status }}</v-card></v-col>
+      <td></td>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    page() {
+      this.$store.state.page = 1;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
